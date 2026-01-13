5afa93238c349faf38cb4e1a772fbbb01d0095e9
LoliPain <burakznec@gmail.com>
2026-01-13 16:08:51 +0400

    flash red on fail

--- src/App.js
+++ src/App.js
@@ -4,12 +4,19 @@ import LifeIndicator from "./components/LifeIndicator";
 
 export default function App() {
   const [lives, setLives] = useState(3);
+  const [visible, setVisible] = useState(false);
+
+  const Overlay = () => {
+    return <div className={`overlay ${visible ? "show" : ""}`}></div>
+  }
+
   const handleAnswer = (isCorrect) => {
     if (!isCorrect) {
       setLives((prev) => {
         const newLives = prev - 1;
         if (newLives <= 0) {
-          window.location.reload();
+          setVisible(true);
+          window.location.reload()
         }
         return newLives;
       });
@@ -18,6 +25,7 @@ export default function App() {
 
   return (
     <>
+      <Overlay />
       <LifeIndicator lives={lives} />
       <Game onAnswer={handleAnswer}/>
       <div className="bg-elems l"></div>
