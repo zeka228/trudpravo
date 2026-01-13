9437a2612870c8e80e095b662e99f302edd99c45
LoliPain <x@lolipa.in>
2026-01-13 15:58:51 +0400

    lives system

--- src/App.js
+++ src/App.js
@@ -1,5 +1,27 @@
+import { useState } from "react";
 import Game from "./components/Game";
+import LifeIndicator from "./components/LifeIndicator";
 
 export default function App() {
-  return <Game />;
+  const [lives, setLives] = useState(3);
+  const handleAnswer = (isCorrect) => {
+    if (!isCorrect) {
+      setLives((prev) => {
+        const newLives = prev - 1;
+        if (newLives <= 0) {
+          window.location.reload();
+        }
+        return newLives;
+      });
+    }
+  };
+
+  return (
+    <>
+      <LifeIndicator lives={lives} />
+      <Game onAnswer={handleAnswer}/>
+      <div className="bg-elems l"></div>
+      <div className="bg-elems r"></div>
+    </>
+  );
 }
