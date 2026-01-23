bd27988d7ca582fab37548f9df35b4ce8b846319
LoliPain <burakznec@gmail.com>
2026-01-23 23:50:27 +0400

    upload new bgs
    apply backgrounds

--- src/App.js
+++ src/App.js
@@ -2,6 +2,9 @@ import { useState } from "react";
 import Game from "./components/Game";
 import LifeIndicator from "./components/LifeIndicator";
 
+import BGImage1 from "./bons/bg_1.jpg";
+import BGImage2 from "./bons/bg_2.jpg";
+
 export default function App() {
   const [lives, setLives] = useState(3);
   const [visible, setVisible] = useState(false);
@@ -28,8 +31,12 @@ export default function App() {
       <Overlay />
       <LifeIndicator lives={lives} />
       <Game onAnswer={handleAnswer}/>
-      <div className="bg-elems l"></div>
-      <div className="bg-elems r"></div>
+      <div className="bg-elems l">
+        <img alt="bg_1.jpg" src={BGImage1}></img>
+      </div>
+      <div className="bg-elems r">
+        <img alt="bg_2.jpg" src={BGImage2}></img>
+      </div>
     </>
   );
 }
