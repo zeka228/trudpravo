4ae99bf1dfa0235064b71b4f3efc0eda0ad84e7e
LoliPain <burakznec@gmail.com>
2026-02-19 03:43:59 +0400

    titleScreen to next step processing
    - fix: Title shld update step only to 1

--- src/components/TitleScreen.jsx
+++ src/components/TitleScreen.jsx
@@ -1,18 +1,16 @@
-import { useState } from "react";
-
 import QRCodes from "./QRCodes";
 
-export default function TitleScreen({}) {
+export default function TitleScreen({ onClick }) {
     return (
-      <>
+      <div className="start-screen">
         <hr className="game-separator"/>
-          <div className="game-title">Квест: Трудовое право</div>
+          <div className="game-title">Квест:<br></br>Трудовое право</div>
           <div className="start-button-wrapper">
-            <div className="start-button">Начать</div>
+            <div className="start-button" onClick={onClick}>Начать</div>
             <span className="start-button-subtitle">10 минут / 20 вопросов / 4 темы</span>
           </div>
           <QRCodes />
         <hr className="game-separator"/>
-      </>
+      </div>
     )
 }
