592f40294cd426da6d300ab205858dbc5e4cab77
LoliPain <x@lolipa.in>
2026-02-19 01:34:47 +0400

    implement titleScreen
    - remove corresponding css

--- src/App.js
+++ src/App.js
@@ -1,33 +1,21 @@
 import { useState } from "react";
 
-import Game from "./components/Game";
+import TitleScreen from "./components/TitleScreen";
 import BGImages from "./components/BGImages";
 
 export default function App() {
-  const [lives, setLives] = useState(3);
-  const [visible, setVisible] = useState(false);
+  const [currentStep, setStep] = useState(0);
 
-  const Overlay = () => {
-    return <div className={`overlay ${visible ? "show" : ""}`}></div>
-  }
-
-  const handleAnswer = (isCorrect) => {
-    if (!isCorrect) {
-      setLives((prev) => {
-        const newLives = prev - 1;
-        if (newLives <= 0) {
-          setVisible(true);
-          window.location.reload()
-        }
-        return newLives;
-      });
+  const CurrentStepComponent = () => {
+    switch (currentStep) {
+      case 0: return <TitleScreen onClick={() => setStep(1)}/>
+      default: return <></>
     }
-  };
+  }
 
   return (
     <>
-      <Overlay />
-      <Game onAnswer={handleAnswer}/>
+      <CurrentStepComponent />
       <BGImages />
     </>
   );
