42f75641dfa58c47730681399ffbad081508a0a6
LoliPain <x@lolipa.in>
2026-02-19 07:17:29 +0400

    count correct answers for scoreboard

--- src/App.js
+++ src/App.js
@@ -15,15 +15,16 @@ import {
 
 export default function App() {
   const [currentStep, setStep] = useState(0);
+  const [validAnswers, setVas] = useState(0);
 
   const CurrentStepComponent = () => {
     switch (currentStep) {
       case 0: return <TitleScreen onClick={() => setStep(1)}/>
-      case 1: return <BaseQuest gameTitle="Трудовое право как отрасль права" questions={questionsLaw} answers={answersLaw} nextStep={() => setStep(2)}/>
-      case 2: return <BaseQuest gameTitle="Дисциплина труда" questions={questionsDiscipline} answers={answersDiscipline} nextStep={() => setStep(3)}/>
-      case 3: return <BaseQuest gameTitle="Трудовой договор и приём на работу" questions={questionsWork} answers={answersWork} nextStep={() => setStep(4)}/>
-      case 4: return <BaseQuest gameTitle="Оплата труда и материальная ответственность" questions={questionsSalary} answers={answersSalary} nextStep={() => setStep(5)}/>
-      case 5: return <ScoreScreen />
+      case 1: return <BaseQuest gameTitle="Трудовое право как отрасль права" questions={questionsLaw} answers={answersLaw} nextStep={() => setStep(2)} vas={setVas}/>
+      case 2: return <BaseQuest gameTitle="Дисциплина труда" questions={questionsDiscipline} answers={answersDiscipline} nextStep={() => setStep(3)} vas={setVas}/>
+      case 3: return <BaseQuest gameTitle="Трудовой договор и приём на работу" questions={questionsWork} answers={answersWork} nextStep={() => setStep(4)} vas={setVas}/>
+      case 4: return <BaseQuest gameTitle="Оплата труда и материальная ответственность" questions={questionsSalary} answers={answersSalary} nextStep={() => setStep(5)} vas={setVas}/>
+      case 5: return <ScoreScreen vas={validAnswers} />
       default: return <></>
     }
   }
