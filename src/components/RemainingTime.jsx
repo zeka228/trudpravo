053e092cc90cbaee49e5ec9408fd3cc8104fa9f5
LoliPain <burakznec@gmail.com>
2026-02-19 04:46:23 +0400

    countdown, starting from step:1

--- src/components/RemainingTime.jsx
+++ src/components/RemainingTime.jsx
@@ -1,7 +1,14 @@
 export default function Timer({ timeLeft }) {
+  const minutes = Math.floor(timeLeft / 60);
+  const seconds = timeLeft % 60;
+
+  const formattedTime = `${minutes.toString().padStart(2, "0")}:${seconds
+    .toString()
+    .padStart(2, "0")}`;
+
   return (
     <div className="timer">
-      <span>Осталось времени:</span><div>{timeLeft}</div>
+      <span>Осталось времени:</span><div>{formattedTime}</div>
     </div>
   );
 }
